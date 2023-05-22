d83608836d00b6ba9a420e2af111712fdd9f659c
STEFANIE VERONICA WIJAYA <134229577+stefanieveronica@users.noreply.github.com>
2023-05-22 09:42:39 +0000

    Update 17 files

--- src/App.js
+++ src/App.js
@@ -7,59 +7,70 @@ import "./styles.css";
 import SwiperCore, {
   EffectCoverflow,
   Pagination,
-  Navigation
+  Navigation,
 } from "swiper/core";
 
 SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="title_wrapper">
-        <div className="reactLogo">
-          <img src="images/react.png" />
-        </div>
-        <div className="title_">
-          <span>React</span>Swiper Slider
+    <div>
+      <div className="image-container">
+        <img src="../images/maps/img1.png" />
+        <div className="text-overlay">
+          <h1>ASCENT</h1>
+          <p>Explore and discover amazing lineups</p>
         </div>
       </div>
-      <Swiper
-        navigation={true}
-        effect={"coverflow"}
-        centeredSlides={true}
-        slidesPerView={window.innerWidth < 768 ? 1 : "auto"}
-        loop={true}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true
-        }}
-        pagination={{
-          clickable: true
-        }}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src="images/1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/6.jpg" />
-        </SwiperSlide>
-      </Swiper>
+      <div className="pentagon"></div>
+      <div className="container">
+        <h1 className="swiper_title">Choose Your Agent</h1>
+        <div className="swiper-container">
+          <Swiper
+            navigation={true}
+            effect={"coverflow"}
+            centeredSlides={true}
+            slidesPerView={window.innerWidth < 768 ? 1 : "auto"}
+            loop={true}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src="images/agents/sova.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="images/agents/brimstone.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="images/agents/cypher.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="images/agents/killjoy.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="images/agents/raze.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="images/agents/breach.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="images/agents/harbor.png" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="images/agents/omen.jpg" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </div>
   );
 }
